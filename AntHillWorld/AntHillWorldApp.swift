5689b52aa9d50fabb83dbe8c1f3eaac95e5c35f8
ivannikov <kyrill2014@mail.ru>
2021-07-13 00:52:25 +0700

    Initial Commit

---
+++ AntHillWorld/AntHillWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  AntHillWorldApp.swift
+//  AntHillWorld
+//
+//  Created by Кирилл Иванников on 13.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct AntHillWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
